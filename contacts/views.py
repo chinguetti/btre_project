5272d545d7fc27ba56c5aa545f47dabf9906e317
Simon Stuart <simon@mail.com>
2019-06-26 10:31:20 +0900

    remove email

--- contacts/views.py
+++ contacts/views.py
@@ -28,10 +28,10 @@ def contact(request):
         #contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
 
-        send_mail('Property Listing inquiry', "There has been an inquiry for " + listing + ". Sign into the admin panel for more info"
-                  , 'jpn.it.dept@gmail.com',
-                  [realtor_email, 'simon@mail.com'],
-                  fail_silently=False)
+        # send_mail('Property Listing inquiry', "There has been an inquiry for " + listing + ". Sign into the admin panel for more info"
+        #           , 'jpn.it.dept@gmail.com',
+        #           [realtor_email, 'simon@mail.com'],
+        #           fail_silently=False)
 
         messages.success(request,'Your reqeust has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
